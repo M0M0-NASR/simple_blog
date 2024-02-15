19bf746b8528f3d664d7aeb0c37cadc750b5a82c
unknown <mohamednasrmahmood@gmail.com>
2024-02-16 00:30:55 +0200

    Update user profile feature

--- database/migrations/2024_02_15_215507_create_users_table.php
+++ database/migrations/2024_02_15_215507_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('img_cover');
+            $table->string('img_cover')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2024_02_15_220508_add_token_to_users.php
+++ database/migrations/2024_02_15_220508_add_token_to_users.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->ulid('token');
+            $table->string('token' , 255);
         });
     }
 
